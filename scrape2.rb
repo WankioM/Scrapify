bec0b0587a8b930dcdb982a4981dd4157f6771ed
WankioM <tracywankio29@gmail.com>
2024-04-09 09:45:46 +0300

    got counrty divs using nokogiri

--- scrape2.rb
+++ scrape2.rb
@@ -1,9 +1,20 @@
 require 'httparty'
+require 'nokogiri'
 
 response = HTTParty.get('https://www.scrapethissite.com/pages/simple/')
 
 if response.code==200
-    puts response.body
+    puts "Success"
 else
     puts " Error #{response.code}"
+end
+
+doc= Nokogiri::HTML4(response.body)
+puts doc
+country_containers = doc.css('div.col-md-4.country')
+puts "next"
+
+country_containers.each do |container|
+    countryname= container.css('h3.country-name')
+    puts countryname
 end

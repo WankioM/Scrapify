d736dc90f7dd67a89e46a43cb8275891c814584d
WankioM <tracywankio29@gmail.com>
2024-04-09 09:56:21 +0300

    parsed using .text

--- scrape2.rb
+++ scrape2.rb
@@ -15,6 +15,14 @@ country_containers = doc.css('div.col-md-4.country')
 puts "next"
 
 country_containers.each do |container|
-    countryname= container.css('h3.country-name')
+    countryname= container.css('h3.country-name').text.strip
     puts countryname
+    countryinfo= container.css('div.country-info')
+    capital = countryinfo.css('span.country-capital').text.strip
+    puts "Capital: #{capital}"
+    population = countryinfo.css('span.country-population').text.strip
+    puts "Population: #{population}"
+    area = countryinfo.css('span.country-area').text.strip
+    puts "Area: #{area}"
+
 end
